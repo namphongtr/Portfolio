d1ad00d3956db47a5b591a1d8aa270ec91ea2a29
ISoaif <phongtran11172006@gmail.com>
2025-12-16 10:20:11 +0100

    thêm tự động tắt vào menu hamburger

--- script/Javascript/main.js
+++ script/Javascript/main.js
@@ -11,6 +11,37 @@ menuButton.addEventListener("click", () => {
   name.classList.toggle("navbar_change_color");
 });
 
+document.addEventListener("click", (e) => {
+  if (
+    menu.classList.contains("navbar__open") &&
+    !menu.contains(e.target) &&
+    !menuButton.contains(e.target)
+  ) {
+    menu.classList.remove("navbar__open");
+    menuButton.classList.remove("open");
+    name.classList.remove("navbar_change_color");
+  }
+});
+
+// menuButton.addEventListener("click", (e) => {
+//   e.stopPropagation();
+//   menu.classList.toggle("navbar__open");
+//   menuButton.classList.toggle("open");
+//   name.classList.toggle("navbar_change_color");
+// });
+
+const Navclose = document.querySelectorAll(".nav-close");
+
+Navclose.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("navbar__open");
+    menuButton.classList.remove("open");
+    name.classList.remove("navbar_change_color");
+  });
+});
+
+//Dark mode
+
 let darkmode = localStorage.getItem("dark_mode");
 const theme_switch = document.getElementById("switch-mode");
 
@@ -21,7 +52,7 @@ const enableDarkmode = () => {
 
 const disableDarkmode = () => {
   document.body.classList.remove("dark_mode");
-  localStorage.removeItem("dark_mode")
+  localStorage.removeItem("dark_mode");
 };
 
 if (darkmode === "active") enableDarkmode();
